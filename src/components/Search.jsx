9c5342d6f2e539a7dd93b1a28f838823f0ca9cbe
ajvejko <moucka.matej01@gmail.com>
2023-01-08 23:12:57 +0100

    resized search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ export default function SearchBar() {
   return (
     <input
       type="text"
-      className="w-2/3 px-5 py-2 rounded-full outline-none border-2 bg-neutral-800 text-white focus:shadow-[0_0px_11px_rgba(0,0,0,0.25)] focus:shadow-blue-500 text-xl"
+      className="w-1/2 px-5 py-2 rounded-full outline-none border-2 bg-neutral-800 text-white focus:shadow-[0_0px_11px_rgba(0,0,0,0.25)] focus:shadow-blue-500 text-xl"
     ></input>
   );
 }
