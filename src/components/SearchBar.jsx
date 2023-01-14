201255a1cc47293c3d49cc93fd49936299d2d464
ajvejko <moucka.matej01@gmail.com>
2023-01-14 21:56:29 +0100

    added space between preview and search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,7 +16,7 @@ export default function SearchBar({ selectedLink }) {
         className="w-full px-5 py-2 rounded-full outline-none border-2 bg-neutral-800 text-white focus:shadow-[0_0px_11px_rgba(0,0,0,0.25)] focus:shadow-blue-500 text-xl"
       ></input>
       {/* Display finalized link */}
-      <div className="text-stone-700 mr-6 text-sm text-right">
+      <div className="text-stone-800 mr-6 mt-1 text-sm text-right">
         {selectedLink ? selectedLink+inputValue : "google.com"}
       </div>
     </div>
