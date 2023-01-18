a3cbc8a3a66bdb8071b0c830b3f1cbeff8777f05
ajvejko <moucka.matej01@gmail.com>
2023-01-18 22:56:56 +0100

    input is now focused on load

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,6 +31,7 @@ export default function SearchBar({ selectedSearch, selectedHome }) {
       {/* Input field for searching */}
       <input
         type="text"
+        autoFocus
         onChange={handleInput}
         className="w-full px-5 py-2 rounded-full outline-none border-2 bg-neutral-800 text-white focus:shadow-[0_0px_11px_rgba(0,0,0,0.25)] focus:shadow-blue-500 text-xl"
       ></input>
